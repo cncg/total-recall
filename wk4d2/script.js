626b585c4b28ee647d81fb19175fe65816b5571f
cncg <ccalvo0314@gmail.com>
2023-07-06 01:34:49 -0400

    part a completed

--- wk4d2/script.js
+++ wk4d2/script.js
@@ -1,15 +1,22 @@
-/*      A
+/*      A - q + a
 ? How do we assign a value to a variable?     you use the let, const or var keywords to declare a variable. However var is not the best practice.
 
-? How do we change the value of a variable?   you change the value of a variable by using the "=" assignment operator , to reassign a new value to a variable 
-? How do we assign an existing variable to a new variable?
-? Remind me, what are declare, assign, and define?
-? What is pseudocoding and why should you do it?
-? What percentage of time should be spent thinking about how you're going to solve a problem vs actually typing in code to solve it?
+? How do we change the value of a variable?   you change the value of a variable by using the "=" assignment operator , to reassign a new value to a variable . For example:
+let favoriteAnimal = "Shark";
+favoriteAnimal = "pig";
 
+? How do we assign an existing variable to a new variable?  we can do this by using the assignment operator again. For example:
+let a = 10;
+let b = a;
+
+? Remind me, what are declare, assign, and define? to declare it means we are declaring a variable, when we use assign it means we are giving that declared variable a value. When you are defining it means you are declaring a variable and assigning an value to to the variable.
+
+? What is pseudocoding and why should you do it?    pseudocoding is the act of writing out code in plain english enough that a non developer could understand, in order to tackle the problem you are going to solve. Pretty much pre planning the code.
+
+? What percentage of time should be spent thinking about how you're going to solve a problem vs actually typing in code to solve it?    you should spent most of the time , i'd say 80% thinking about how to tackle the problem , the rest will be coding and finding the bugs along the way while working live, to be more efficient
 */
 
-/*      B
+/*      B - Strings
 Create a variable called firstVariable
 Assign it the value of the string "Hello World"
 Change the value of this variable to some number
