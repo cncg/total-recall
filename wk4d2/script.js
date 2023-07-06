62a0554c1b5c7d054e5be8dd7c04cf80789ed6a6
cncg <ccalvo0314@gmail.com>
2023-07-06 00:47:09 -0400

    initial commit

---
+++ wk4d2/script.js
@@ -0,0 +1,32 @@
+/*      A
+? How do we assign a value to a variable?     you use the let, const or var keywords to declare a variable. However var is not the best practice.
+
+? How do we change the value of a variable?   you change the value of a variable by using the "=" assignment operator , to reassign a new value to a variable 
+? How do we assign an existing variable to a new variable?
+? Remind me, what are declare, assign, and define?
+? What is pseudocoding and why should you do it?
+? What percentage of time should be spent thinking about how you're going to solve a problem vs actually typing in code to solve it?
+
+*/
+
+/*      B
+Create a variable called firstVariable
+Assign it the value of the string "Hello World"
+Change the value of this variable to some number
+Store the value of firstVariablein a new variable called secondVariable
+Change the value of secondVariableto any string.
+What is the value of firstVariable?
+Create a variable called yourNameand set it equal to your name as a string. Then, write an expression that takes the string "Hello, my name is " and the variable yourNameso that it returns a new string with them concatenated.
+ex: Hello, my name is Jean Valjean
+*/
+
+// D. The farm
+
+let animal = "2";
+
+if (animal === "") {
+  console.log("You're not a cow");
+} else animal === "cow";
+{
+  console.log("moooooooo");
+}
