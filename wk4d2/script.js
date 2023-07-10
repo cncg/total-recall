c040ab815a24bf319a4f4f776541b1b21742ab0a
cncg <ccalvo0314@gmail.com>
2023-07-10 17:32:11 -0400

    PT4 A- printgreeting complete

--- wk4d2/script.js
+++ wk4d2/script.js
@@ -230,73 +230,79 @@
 // } else {
 //   console.log("monkey");
 // }
+
+// // * ==============================================================================
+// // ? PT.3 H-  What's in Your Closet?
+// // ? What's Kristyn wearing today? Using bracket notation to access items in kristynsCloset, log the sentence "Kristyn is rocking that " + the third item in Kristyn's closet + " today!" to the console.
+// // ? Kristyn just bought some sweet shades! Add "raybans"to her closet after "yellow knit hat".
+// // ? Kristyn spilled coffee on her hat... modify this item to read "stained knit hat"instead of yellow.
+// // ? Put together an outfit for Thom! Using bracket notation, access the first element in Thom's shirtsarray
+// // ? In the same way, access one item from Thom's pants array.
+// // ? Access one item from Thom's accessories array.
+// // ? Log a sentence about what Thom's wearing. Example: "Thom is looking fierce in a grey button-up, jeans and wool scarf!"
+// // ? Get more specific about what kind of PJs Thom's wearing this winter. Modify the name of his PJ pants to Footie Pajamas.
+
+// const kristynsCloset = [
+//   "left shoe",
+//   "cowboy boots",
+//   "right sock",
+//   "Per Scholas hoodie",
+//   "green pants",
+//   "yellow knit hat",
+//   "marshmallow peeps",
+// ];
+
+// // Thom's closet is more complicated. Check out this nested data structure!!
+// const thomsCloset = [
+//   [
+//     // These are Thom's shirts
+//     "grey button-up",
+//     "dark grey button-up",
+//     "light blue button-up",
+//     "blue button-up",
+//   ],
+//   [
+//     // These are Thom's pants
+//     "grey jeans",
+//     "jeans",
+//     "PJs",
+//   ],
+//   [
+//     // Thom's accessories
+//     "wool mittens",
+//     "wool scarf",
+//     "raybans",
+//   ],
+// ];
+
+// // logging out Kristyn's third item
+// console.log("Kristyn is rocking that " + kristynsCloset[2] + " today!");
+// kristynsCloset.splice(6, 0, "rayban"); // adding "rayban" after the 5th indexed element in the array
+// kristynsCloset.splice(5, 1, "stained knit hat"); // changes the 5th indexed element from yellow knit hat to stained knit hat
+// // accessing Thom's 1st element
+// console.log(thomsCloset[0][0]);
+// // any item from Thom's pants
+// console.log(thomsCloset[1][1]);
+// // any of thoms accessories
+// console.log(thomsCloset[2][1]);
+// // logging what thoms wearing
+// console.log(
+//   "Thom is looking fierce in a " +
+//     thomsCloset[0][0] +
+//     ", " +
+//     thomsCloset[1][1] +
+//     " & " +
+//     thomsCloset[2][1] +
+//     "!"
+// );
+
+// thomsCloset[1][2] = "Footie Pajamas";
+// console.table(thomsCloset);
+
 // * ==============================================================================
-// ? PT.3 H-  What's in Your Closet?
-// ? What's Kristyn wearing today? Using bracket notation to access items in kristynsCloset, log the sentence "Kristyn is rocking that " + the third item in Kristyn's closet + " today!" to the console.
-// ? Kristyn just bought some sweet shades! Add "raybans"to her closet after "yellow knit hat".
-// ? Kristyn spilled coffee on her hat... modify this item to read "stained knit hat"instead of yellow.
-// ? Put together an outfit for Thom! Using bracket notation, access the first element in Thom's shirtsarray
-// ? In the same way, access one item from Thom's pants array.
-// ? Access one item from Thom's accessories array.
-// ? Log a sentence about what Thom's wearing. Example: "Thom is looking fierce in a grey button-up, jeans and wool scarf!"
-// ? Get more specific about what kind of PJs Thom's wearing this winter. Modify the name of his PJ pants to Footie Pajamas.
+// ?  PT.4 Functions A- printGreeting
 
-const kristynsCloset = [
-  "left shoe",
-  "cowboy boots",
-  "right sock",
-  "Per Scholas hoodie",
-  "green pants",
-  "yellow knit hat",
-  "marshmallow peeps",
-];
-
-// Thom's closet is more complicated. Check out this nested data structure!!
-const thomsCloset = [
-  [
-    // These are Thom's shirts
-    "grey button-up",
-    "dark grey button-up",
-    "light blue button-up",
-    "blue button-up",
-  ],
-  [
-    // These are Thom's pants
-    "grey jeans",
-    "jeans",
-    "PJs",
-  ],
-  [
-    // Thom's accessories
-    "wool mittens",
-    "wool scarf",
-    "raybans",
-  ],
-];
-
-// logging out Kristyn's third item
-console.log("Kristyn is rocking that " + kristynsCloset[2] + " today!");
-kristynsCloset.splice(6, 0, "rayban"); // adding "rayban" after the 5th indexed element in the array
-kristynsCloset.splice(5, 1, "stained knit hat"); // changes the 5th indexed element from yellow knit hat to stained knit hat
-// accessing Thom's 1st element
-console.log(thomsCloset[0][0]);
-// any item from Thom's pants
-console.log(thomsCloset[1][1]);
-// any of thoms accessories
-console.log(thomsCloset[2][1]);
-// logging what thoms wearing
-console.log(
-  "Thom is looking fierce in a " +
-    thomsCloset[0][0] +
-    ", " +
-    thomsCloset[1][1] +
-    " & " +
-    thomsCloset[2][1] +
-    "!"
-);
-
-thomsCloset[1][2] = "Footie Pajamas";
-console.table(thomsCloset);
-
-
-// * ==============================================================================
+function printGreeting(name) {
+  console.log(`Hello there, ${name}!`);
+}
+printGreeting("Slimer");
